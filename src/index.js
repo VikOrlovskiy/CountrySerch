6b279d72803733f89d1661e4d4c09b9eb181da36
vik_orel <vikorel34@gmail.com>
2021-11-28 22:23:43 +0200

    1st commit js and fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
-
+import CountriesApiService from './js/fetchCountries';
+// =====================================================
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+const Refs = {
+  input: document.getElementById('search-box'),
+  list: document.querySelector('.country-list'),
+  div: document.querySelector('.country-info'),
+};
+// ======================================================
+Refs.input.addEventListener('input', debounce(updateValue, DEBOUNCE_DELAY));
+const countriesApiService = new CountriesApiService();
+
+function updateValue(e) {
+  console.log(e.target.value);
+  countriesApiService.value = e.target.value;
+  countriesApiService.fetchCountries().then(countries => {
+    createCountyCard(countries);
+  });
+}
+// ======================================================
+function createCountyCard(countries) {
+  return countries.map(({ name, capital, population, flags, languages }) => {
+    // Refs.list.innerHTML = { name, capital, population, flags, languages };
+    return console.log({ name, capital, population, flags, languages });
+  });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,27 @@
+import Notiflix from 'notiflix';
+export default class DataFetch {
+  constructor() {
+    this.searchValue = '';
+    console.log(this.searchValue);
+  }
+  fetchCountries() {
+    const URL = `https://restcountries.com/v3.1/name/${this.searchValue}?fields=name,capital,population,flags,languages,`;
+    return fetch(URL)
+      .then(response => response.json())
+      .then(countries => {
+        if (countries.status === 404) {
+          return Notiflix.Notify.failure(`"Oops, there is no country with that name"`);
+        } else if (countries.length > 10) {
+          return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        }
+        return countries;
+      })
+      .catch(e => {});
+  }
+  get value() {
+    return this.searchValue;
+  }
+  set value(country) {
+    this.searchValue = country;
+  }
+}
